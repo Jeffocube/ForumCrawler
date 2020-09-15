9854f9ec6ed6e8978aa022f37d2ca7439aa203b3
Jeff <jlai030@ucr.edu>
2020-09-15 16:45:15 -0700

    New site

---
+++ centipede/module/ubcbot_user.py
@@ -0,0 +1,37 @@
+
+
+http_rules = {
+    'method'  : 'GET',
+    'page' : {
+        'key'   : 'page',
+        'value' : 1,
+    },
+    'stop' : lambda x: len(x.xpath('//*[@id="content"]/div/div/div[3]/div[2]/nav/a[last()][contains(@class,"text")]')) == 0
+    #'stop' :  lambda x: int(x.xpath('//*[@id="content"]/div/div/div[3]//a[contains(@class,"currentPage")]/text()')[0]) == 2,
+    # 'stop' : lambda x: True,
+}
+elements = {
+    'path': '//*[contains(@class, "message ")]',
+    'attributes': {
+        'messageID': {
+            'attrib': 'id',
+            'regex' : r'\d+'},
+        'mainText': {
+            'path': './div[2]/div[1]/article/blockquote/text()',
+            'attrib': 'text',
+            'regex' : '[^\n\t]+',},
+        'mainTextLinks': {
+            'path': './div[2]/div[1]/article/blockquote/a',
+            'attrib': 'href',},
+        'username': {
+            'path': './div[1]/div/h3/a/text()',
+            'attrib': 'text',},
+        'dateTime': {
+            'path': './div[2]/div[contains(@class,"messageMeta")]/div[1]/span/a/span',
+            'attrib': 'title',},
+        'quoteLink': {
+            'path': './div[1]/article/blockquote/a',
+            'attrib': 'href',
+            'regex' : r'[\d]+$',},
+    }
+}
